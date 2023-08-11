e3b2793f4bddf3ef3bf2771ef4083bb803d02b26
Guyae <guy.apisit2546@gmail.com>
2023-08-11 16:03:09 +0700

    added some fn

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    pass
+
+def welcome():
+    pass
+
+def pull_data():
+    pass
+
+def added_data():
+    pass
+
+def sort_data():
+    pass
+
+def calculate_score():
+    pass
+
+def check_grade():
+    pass
+
+if __name__ == '__main__':
+    main()
