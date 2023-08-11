ef5b7cf3f7de7e01a8385f2b7fbde722c3571fe2
Guyae <guy.apisit2546@gmail.com>
2023-08-12 00:12:57 +0700

    Add detail in def welcome

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def main():
-    pass
+def main(): 
 
 def welcome():
-    print("-------------------- Calculate Grade Programing --------------------")
+    print('''-------------------- Calculate Grade Program --------------------
+                Choose subject :
+                1. 618240-165 DATA STRUCTURES AND ALGORITHMS
+                2. 618214-165 ELECTRICAL ENGINEERING MATHEMATICS II
+                3. 618222-165 ELECTRIC CIRCUIT ANALYSIS
+                4. 618224-165 ELECTRONIC DEVICES AND CIRCUIT DESIGN
+                5. 618250-165 DIGITAL CIRCUITS AND LOGIC DESIGN
+                6. GPA
+                7. Quit''')
+    choice = int(input("Enter your subject :"))
+    return choice
+
+def ask_pull_or_add():
     pass
 
 def pull_data():
@@ -22,5 +33,7 @@ def check_grade():
 
 if __name__ == '__main__':
     main()
-    welcome()
+    choice = welcome()
+    print(choice)
+    
     
