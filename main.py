a541869c534301af3603fc758c2c428aa10e8762
Guyae <guy.apisit2546@gmail.com>
2023-08-13 00:34:02 +0700

    Add if else
    Add if else for define filename and choose fn

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 def main(): 
-    pass
+    choice = welcome()
+    if choice == 1:
+        filename = "test.csv"
+    elif choice == 2:
+        filename = "618214.csv"
+    elif choice == 3:
+        filename = "618222.csv"
+    elif choice == 4:
+        filename = "618224.csv"
+    elif choice == 5:
+        filename = "618250.csv"
+    elif choice == 6:
+        filename = "GPA.csv"
+    
+    ask_pull_add = input("Do you want to pull/add : ").lower
+    if ask_pull_add == 'pull':
+        pull_data(filename)
+    elif ask_pull_add == 'add':
+        added_data(filename)
 
 def welcome():
     print('''-------------------- Calculate Grade Program --------------------
@@ -11,13 +29,14 @@ def welcome():
                 5. 618250-165 DIGITAL CIRCUITS AND LOGIC DESIGN
                 6. GPA
                 7. Quit''')
-    choice = int(input("Enter your subject :"))
-    return choice
+    try:
+        choice = int(input("Enter your subject :"))
+        return choice
 
-def pull_data():
+def pull_data(filename):
     pass
 
-def added_data():
+def added_data(filename):
     pass
 
 def sort_data():
