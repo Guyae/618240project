42fb679e60740635fc9386948fa73b0307d2b53d
Guyae <guy.apisit2546@gmail.com>
2023-08-22 17:28:26 +0700

    แก้ตลอดไป

--- main.py
+++ main.py
@@ -372,11 +372,10 @@ or enter 'Quit' for close''')
             degree_product_grade += number_grade*degree[calculate-1]    # เอาเกรดที่เป็นตัวเลข*หน่วยกิต 
             if Grade[grades][calculate] != '-':                         # เช็คเกรดที่นำเข้ามา ไม่ใช่ '-' ใช่มั้ย
                 sum_degree += degree[calculate-1]                       # sum_degree หรือผลรวมหน่วยกิต จะบวกหน่วยกิตวิชานั้นๆ
-        try:                                                            # เช็คว่า หารได้หรือไม่
-            GPA = "{:.2f}".format(degree_product_grade/sum_degree)                      # สุตรคำนวณ GPA คือผลรวมของเกรดที่ได้*หน่วยกิตวิชานั้นๆ หารด้วย ผลรวมหน่วยกิต
-        except ZeroDivisionError:                                       # ถ้าเป็น 0/0 จะ error 
-            GPA = 0                                                     # จึงกำหนดให้ GPA = 0
+        GPA = "{:.2f}".format(degree_product_grade/sum_degree)          # สุตรคำนวณ GPA คือผลรวมของเกรดที่ได้*หน่วยกิตวิชานั้นๆ หารด้วย ผลรวมหน่วยกิต
         Grade[grades][6] = GPA                                          # นำ GPA ไปใส่ใน Grade 
+        degree_product_grade = 0                                        # กำหนดให้ degree_product_grade = 0 เพื่อเริ่มรอบใหม่
+        sum_degree = 0                                                  # กำหนดให้ sum_degree = 0 เพื่อเริ่มรอบใหม่
 
     # print(Grade)
 
